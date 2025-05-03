4e466ecfc4b873443f48944dce052a536f06c513
TrinhVanManh129 <hoanglong129148@gmail.com>
2025-05-03 03:29:41 -0700

    Fix lỗi cors

--- backend/src/main/java/backend/backend/config/SecurityConfig.java
+++ backend/src/main/java/backend/backend/config/SecurityConfig.java
@@ -2,14 +2,15 @@ package backend.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-// @EnableWebSecurity
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -17,19 +18,16 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    // @Bean
-    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    //     http
-    //         .csrf(csrf -> csrf.disable())
-    //         .cors(withDefaults())
-    //         .authorizeHttpRequests(authz -> authz
-    //             // Các API admin cần được bảo vệ
-    //             // .requestMatchers(new AntPathRequestMatcher("/api/admin/**")).hasRole("ADMIN")
-    //             // Tạm thời cho phép tất cả các request để dễ dàng test
-    //             .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
-    //         )
-    //         .httpBasic(withDefaults());
-
-    //     return http.build();
-    // }
-}
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .cors(withDefaults())
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
+            )
+            .httpBasic(withDefaults());
+        
+        return http.build();
+    }
+} 
